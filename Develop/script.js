a7b66c7403ab213c0fc3e87ba130133836b5a7e4
Joanna Preston <joannappreston@gmail.com>
2020-03-17 16:03:16 -0700

    Added method to input object to make sure that user chose at least one password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -4,23 +4,37 @@ function generatePassword() {
 
   // Create an object to store user input in seperate properties
   var input = {
+    length: prompt("Password length: "),
     lowercase : confirm("Do you want to include lowercase letters?"),
     uppercase : confirm("Do you want to include uppercase letters?"),
     numbers : confirm("Do you want to include numbers?"),
     special : confirm("Do you want to include special characters?"),
-    length: prompt("Password length: "),
 
     // Object method to check input length
     checkLength : function() {
       if (this.length < 8 || this.length > 128) {
         alert("Password must be at least 8 characters in length");
-        length = prompt("Password length: ");
+        this.length = prompt("Password length: ");
       };
+    },
+
+    // Object method to check that user selected at least one character type
+    checkForInput : function () {
+      if (this.lowercase === false && this.uppercase === false && this.numbers === false && this.special === false) {
+        alert("You must choose at least 1 criteria");
+        this.lowercase = confirm("Do you want to include lowercase letters?");
+        this.uppercase = confirm("Do you want to include uppercase letters?");
+        this.numbers = confirm("Do you want to include numbers?");
+        this.special = confirm("Do you want to include special characters?");
+      }
     }
   };
 
   // Calls method to check inputed password length
   input.checkLength();
+
+  // Calls method to check if user chose any criteria
+  input.checkForInput();
 
   // Create an object to store possible character sets based on user input
   var choices = {
@@ -59,8 +73,8 @@ function generatePassword() {
     for (var i = 0; i < length; i++) {
       result += choicesSet[Math.floor(Math.random() * choicesSet.length)];
     }
-      // Returns randomly generated password
-      return result;
+    // Returns randomly generated password
+    return result;
   };
 
   // Returns the outputed value of genPassword with the user input length passed in as an argument
